7bfd6b00dec1a09be7a1e843d9b0171ab7875abb
Debangshu Nath <thecrusader.25225@gmail.com>
2024-08-10 17:40:53 +0530

    Update Slideshow.js

--- src/Slideshow.js
+++ src/Slideshow.js
@@ -9,6 +9,7 @@ export default function Slideshow({
 }) {
   const [currentIndex, setCurrentIndex] = useState(0);
   const navigate = useNavigate();
+
   useEffect(() => {
     const slideInterval = setInterval(() => {
       setCurrentIndex((prevIndex) =>
@@ -32,7 +33,7 @@ export default function Slideshow({
   };
 
   return (
-    <div className="w-full h-full overflow-hidden relative ">
+    <div className="w-full h-full overflow-hidden relative">
       {images.map((image, index) => (
         <div
           key={index}
@@ -40,16 +41,27 @@ export default function Slideshow({
             index === currentIndex
               ? "opacity-100 pointer-events-auto"
               : "opacity-0 pointer-events-none"
-          } duration-1000 p-8 cursor-pointer`}
-          onClick={() => handleClick(index)}
+          } duration-1000`}
         >
           <img
             src={image}
             alt="slide"
-            className="w-full h-full object-cover  hover:scale-105 duration-200"
+            className="w-full h-full object-cover hover:scale-105 duration-200 cursor-pointer"
+            onClick={() => handleClick(index)}
           />
         </div>
       ))}
+      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2 z-20">
+        {images.map((_, index) => (
+          <div
+            key={index}
+            className={`w-3 h-3 rounded-full ${
+              index === currentIndex ? "bg-white" : "bg-gray-600"
+            } cursor-pointer`}
+            onClick={() => setCurrentIndex(index)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
